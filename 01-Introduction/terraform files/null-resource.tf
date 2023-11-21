3b5487c8e3d1efc4a0afa5ef35d37a8d76ae8cc0
sappiah110 <142369546+sappiah110@users.noreply.github.com>
2023-11-20 22:04:07 -0500

    null-resource.tf

--- 01-Introduction/terraform files/null-resource.tf
+++ 01-Introduction/terraform files/null-resource.tf
@@ -7,25 +7,32 @@ resource "null_resource" "vscode-config" {
     host        = aws_instance.ubuntu.public_ip
     user        = "ubuntu"
     password    = ""
-    private_key = file("~/Downloads/ansible-key.pem")
+    private_key = file("~/Downloads/mynewkey.pem")
+  }
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
   }
 
   provisioner "remote-exec" {
-    script = "script.sh"
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh", # Remove the spurious CR characters.
+      "sudo /tmp/script.sh",
+    ]
   }
+
+  #provisioner "remote-exec" {
+  #   script = "sudo bash script.sh"
+  # }
 
   provisioner "local-exec" {
     command = templatefile("${var.os}-ssh-script.tpl", {
-      hostname     = aws_instance.ubuntu.public_ip
+      hostname     = aws_instance.ubuntu.public_ip,
       user         = "ansible",
-      IdentityFile = "~/Downloads/ansible-key.pem"
+      IdentityFile = "~/Downloads/LM-Class32.pem"
     })
     interpreter = var.os == "windows" ? ["powershell", "-Command"] : ["bash", "-c"]
   }
-}
 
-resource "time_sleep" "wait_for_instance" {
-  create_duration = "180s"
-
-  depends_on = [aws_instance.ubuntu]
 }
